93d5ac0f83d96fbc487a02564887035e4f703631
PabloRg24 <195672403+PabloRg24@users.noreply.github.com>
2026-06-17 21:44:36 +0200

    feat: serve frontend from Go backend

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -38,3 +38,6 @@ func main() {
 	log.Println("Server running on :8080")
 	http.ListenAndServe(":8080", r)
 }
+
+fs := http.FileServer(http.Dir("./frontend"))
+r.Handle("/*", http.StripPrefix("/", fs))
